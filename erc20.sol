a91a4cef9d881e95b152cba1f3059ffbdaff9a3e
Aman403 <80942430+Aman403@users.noreply.github.com>
2022-06-14 17:06:04 +0530

    Create erc20.sol

---
+++ erc20.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.8.10;
+interface IERC20{
+    function totalSupply() external view returns(uint);
+
+    function balanceOf(address account) external view returns(uint);
+
+    function tranfer(address recipient,uint amount) external returns(bool);
+
+    function allowance(address owner,address spender) external view returns(uint);
+
+    function approve(address spender,uint amount) external returns(bool);
+
+    function transferFrom(address sender,
+    address recipient,
+    uint amount) external returns(bool);
+
+    event Tranfer(address indexed from, address indexed to,uint amount);
+    event Approval(address indexed owner,address indexed spender,uint amount);
+
+}
+
+contract ERC20 is IERC20{
+    uint public totalSupply;
+    mapping(address=>uint) public balanceOf;
+    mapping(address=>mapping(address=>uint)) public allowance;
+    string public name="Test";
+    string public symbol="TEST";
+    uint8 public decimals=18;
+
+    function tranfer(address recipient,uint amount) external returns(bool){
+        balanceOf[msg.sender]-=amount;
+        balanceOf[recipient]+=amount;
+        emit Tranfer(msg.sender,recipient,amount);
+        return true;
+    }
+
+    function approve(address spender,uint amount) external returns(bool){
+        allowance[msg.sender][spender]=amount;
+        emit Approval(msg.sender,spender,amount);
+        return true;
+    }
+
+    function transferFrom(address sender,
+    address recipient,
+    uint amount) external returns(bool){
+
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient]+=amount;
+        emit Tranfer(sender,recipient,amount);
+        return true;
+    }
+    
+    function mint(uint amount) external{
+        balanceOf[msg.sender]+=amount;
+        totalSupply+=amount;
+        emit Tranfer(address(0),msg.sender,amount);
+    }
+
+    function burn(uint amount) external{
+        balanceOf[msg.sender]-=amount;
+        totalSupply-=amount;
+        emit Tranfer(msg.sender,address(0),amount);
+    }
+}
